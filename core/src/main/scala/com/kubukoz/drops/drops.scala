abf281e4301a9c01a244fd0e77dc1ca206b0d9e6
Jakub Kozłowski <kubukoz@gmail.com>
2021-06-10 01:52:37 +0200

    Change primitives, remove `union` for now

--- core/src/main/scala/com/kubukoz/drops/drops.scala
+++ core/src/main/scala/com/kubukoz/drops/drops.scala
@@ -18,40 +18,61 @@ object drops {
   }
 
   trait Schema[A] {
-    def both(a: A): List[(String, String)]
-
-    def headers(a: A): List[String] = both(a).map(_._1)
-    def cells(a: A): List[String] = both(a).map(_._2)
+    def headers: List[String]
+    def cells(a: A): List[String]
 
     def render: Render[A] = Render.nested(this)
+    def provide(a: A): Schema[Any] = Schema.instance(headers, _ => cells(a))
   }
 
   object Schema {
+
+    def instance[A](headers: List[String], cells: A => List[String]): Schema[A] = {
+      val h = headers
+      val c = cells
+      new Schema[A] {
+        val headers: List[String] = h
+
+        def cells(a: A): List[String] = c(a)
+      }
+    }
+
     def make[A](mk: Column[A] => Schema[A]): Schema[A] = mk(new Column[A])
-    def union[A](mk: A => Schema[Unit]): Schema[A] = mk(_).both(())
 
     implicit def semigroup[A]: Semigroup[Schema[A]] =
-      (a, b) => c => a.both(c) ++ b.both(c)
-
-    def single[A](label: String)(value: A)(render: Render[A]): Schema[Unit] =
-      _ =>
-        List(
-          (label, render.render(value))
+      (a, b) =>
+        instance(
+          a.headers ++ b.headers,
+          s => a.cells(s) ++ b.cells(s),
         )
+
+    def single[A](label: String)(value: A)(render: Render[A]): Schema[Unit] = {
+      val rendered = List(render.render(value))
+
+      instance(
+        List(label),
+        _ => rendered,
+      )
+    }
 
   }
 
   final class Column[A] private[drops] () {
+
     def apply[B](label: String)(f: A => B)(render: Render[B]): Schema[A] =
-      a => Schema.single(label)(f(a))(render).both(())
+      Schema.instance(
+        List(label),
+        a => List(render.render(f(a))),
+      )
+
   }
 
   private def cleanupColorCodes(s: String): String =
     s.replaceAll("\u001B\\[[;\\d]*m", "")
 
   def drawTable[A](rows: NonEmptyList[A])(schema: Schema[A]): Seq[String] = {
+    val headers = schema.headers
     val renderedRows = rows.map(schema.cells).toList
-    val headers = schema.headers(rows.head)
     val widths = (headers, renderedRows.transpose)
       .parMapN { (header, valuesAtHeader) =>
         (header :: valuesAtHeader)
